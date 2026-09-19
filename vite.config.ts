3da32c2e35d9d03f083c98a9f96d1ab361fa704c
Mohamed-Shamil <mohamedshamilvadakkan@gmail.com>
2026-09-19 11:47:36 +0530

    ci: configure GitHub Pages automated workflow and base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cater-flow/',
   server: {
     port: 5175,
     host: true
